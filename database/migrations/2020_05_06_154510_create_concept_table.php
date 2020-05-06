47be0707a33fad98dce390068d68f52cdd2bbf0e
Elbaz Michael <elbazmichael92@gmail.com>
2020-05-06 21:55:10 +0200

    Add metadata table

--- database/migrations/2020_05_06_154510_create_concept_table.php
+++ database/migrations/2020_05_06_154510_create_concept_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use App\Util\KeyUtils;
 
 class CreateConceptTable extends Migration
 {
@@ -14,16 +15,18 @@ class CreateConceptTable extends Migration
      */
     public function up()
     {
+
         Schema::create('concept', function (Blueprint $table) {
             $table->id();
             $table->string('vocabulary_id', 15)->index();
             $table->string('concept_code', 15)->index();
+            $table->string('vocabulary_id_concept_code', 15 * 2)->virtualAs($this->concatKey())->index();
             $table->string('concept_name');
-            $table->string('standard_concept')->index();
+            $table->boolean('standard_concept')->default(false)->index();
             $table->string('concept_name_hash', 32)->virtualAs('MD5(concept_name)');
-            $table->boolean('score')->index();
+            $table->boolean('score')->default(false)->index();
             $table->date('start_date')->index();
-            $table->date('end_date')->index();
+            $table->date('end_date')->index()->nullable();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
 
@@ -39,5 +42,10 @@ class CreateConceptTable extends Migration
     public function down()
     {
         Schema::dropIfExists('concept');
+    }
+
+    private function concatKey()
+    {
+        return sprintf("CONCAT(vocabulary_id, '%s', concept_code)", KeyUtils::SEPARATOR);
     }
 }
---
+++ database/migrations/2020_05_06_190339_create_metadata_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMetadataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('metadata', function (Blueprint $table) {
+            $table->id();
+            $table->string('concept_id', 15 * 2)->index();
+            $table->string('name', 30)->index();
+            $table->string('value', 500);
+            $table->date('start_date')->index();
+            $table->date('end_date')->index()->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('metadata');
+    }
+}
