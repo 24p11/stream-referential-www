f6f05c8415242b467e72a92f6740ad934bd1f396
Elbaz Michael <elbazmichael92@gmail.com>
2020-05-11 12:38:37 +0200

    Add env parameter for swagger url

--- resources/views/admin/referential/index.blade.php
+++ resources/views/admin/referential/index.blade.php
@@ -4,5 +4,6 @@
     <div class="jumbotron">
         <h1 class="display-4">Administration</h1>
         <p>L'interface d'administration vous permet de gérer les référentiels.</p>
+        <a href="/{{ env('L5_SWAGGER_ROUTE') }}">Documentation api rest</a>
     </div>
 @endsection
