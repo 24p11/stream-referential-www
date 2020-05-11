25c887d20c3cad2cdeb3de2f4af36796ce0a5b99
Elbaz Michael <elbazmichael92@gmail.com>
2020-05-11 13:19:04 +0200

    Custom errors page
    - 404
    - 500

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,17 @@
-404
+@extends('layouts.base_errors')
+
+@section('content')
+    <br>
+    <br>
+    <div class="container-fluid text-center">
+        <img src="{{ asset('img/404.png') }}"/>
+        <br>
+        <br>
+        <p>
+            La page que vous recherchez n'existe pas.
+        </p>
+        <a class="back-previous-page" href="/">
+            Revenir a l'accueil
+        </a>
+    </div>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.base_errors')
+
+@section('content')
+    <br>
+    <br>
+    <div class="container-fluid text-center">
+        <img src="{{ asset('img/500.png') }}"/>
+        <br>
+        <br>
+        <p>
+            Une erreur serveur s'est produite.
+        </p>
+    </div>
+@endsection
