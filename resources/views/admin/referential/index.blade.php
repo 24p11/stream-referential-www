87fa05d1c50849bf74687b5f1898728704748383
Elbaz Michael <elbazmichael92@gmail.com>
2020-05-12 14:25:18 +0200

    Swagger new tab

--- resources/views/admin/referential/index.blade.php
+++ resources/views/admin/referential/index.blade.php
@@ -4,6 +4,6 @@
     <div class="jumbotron">
         <h1 class="display-4">Administration</h1>
         <p>L'interface d'administration vous permet de gérer les référentiels.</p>
-        <a href="/{{ env('L5_SWAGGER_ROUTE') }}">Documentation api rest</a>
+        <a href="/{{ env('L5_SWAGGER_ROUTE') }}" target="_blank">Documentation api rest</a>
     </div>
 @endsection
