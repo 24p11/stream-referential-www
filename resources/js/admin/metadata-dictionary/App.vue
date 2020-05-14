9f4a79136e05cb2d0f14995ff3e92e729563af91
Elbaz Michael <elbazmichael92@gmail.com>
2020-05-14 14:56:59 +0200

    Empty metadataDictionaries case

--- resources/js/admin/metadata-dictionary/App.vue
+++ resources/js/admin/metadata-dictionary/App.vue
@@ -1,41 +1,49 @@
 <template>
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Variable</th>
-            <th scope="col">Déscription</th>
-            <th scope="col">Type</th>
-            <th scope="col">Date de début</th>
-            <th scope="col">Date de fin</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="metadataDictionary in metadataDictionaries">
-            <td><span class="badge badge-primary">{{metadataDictionary.vocabulary_id}}-{{metadataDictionary.id}}</span>
-            </td>
-            <td>
-                {{ metadataDictionary.metadata_name }}
-            </td>
-            <td>
-                <AutoSaveInput :metadata-dictionary-id="metadataDictionary.id" :value="metadataDictionary.description"
-                               name="description"/>
-            </td>
-            <td>
-                <AutoSaveInput :metadata-dictionary-id="metadataDictionary.id" :value="metadataDictionary.type"
-                               name="type"/>
-            </td>
-            <td>
-                <AutoSaveInput :metadata-dictionary-id="metadataDictionary.id" :value="metadataDictionary.start_date"
-                               name="start_date"/>
-            </td>
-            <td>
-                <AutoSaveInput :metadata-dictionary-id="metadataDictionary.id" :value="metadataDictionary.end_date"
-                               name="end_date"/>
-            </td>
-        </tr>
-        </tbody>
-    </table>
+    <div>
+        <table class="table" v-if="hasVariables">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Variable</th>
+                <th scope="col">Déscription</th>
+                <th scope="col">Type</th>
+                <th scope="col">Date de début</th>
+                <th scope="col">Date de fin</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="metadataDictionary in metadataDictionaries">
+                <td><span
+                    class="badge badge-primary">{{metadataDictionary.vocabulary_id}}-{{metadataDictionary.id}}</span>
+                </td>
+                <td>
+                    {{ metadataDictionary.metadata_name }}
+                </td>
+                <td>
+                    <AutoSaveInput :metadata-dictionary-id="metadataDictionary.id"
+                                   :value="metadataDictionary.description"
+                                   name="description"/>
+                </td>
+                <td>
+                    <AutoSaveInput :metadata-dictionary-id="metadataDictionary.id" :value="metadataDictionary.type"
+                                   name="type"/>
+                </td>
+                <td>
+                    <AutoSaveInput :metadata-dictionary-id="metadataDictionary.id"
+                                   :value="metadataDictionary.start_date"
+                                   name="start_date"/>
+                </td>
+                <td>
+                    <AutoSaveInput :metadata-dictionary-id="metadataDictionary.id" :value="metadataDictionary.end_date"
+                                   name="end_date"/>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <div class="alert alert-primary" role="alert" v-else>
+            Aucune variables pour le référentiel {{referential}}.
+        </div>
+    </div>
 </template>
 
 <script>
@@ -45,6 +53,7 @@
     export default {
         data() {
             return {
+                referential: referential,
                 metadataDictionaries: []
             }
         },
@@ -52,6 +61,11 @@
             axios
                 .get(apiUrl)
                 .then(response => this.metadataDictionaries = response.data)
+        },
+        computed: {
+            hasVariables() {
+                return this.metadataDictionaries.length > 0
+            }
         },
         components: {
             AutoSaveInput
